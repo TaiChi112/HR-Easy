977e1a2582ccaf03c5fc435f496fa7d6b24c52ca
TaiChi <anothai.0978452316@gmail.com>
2026-05-26 22:16:07 +0700

    feat: add authentication configuration and NextAuth setup

---
+++ auth.ts
@@ -0,0 +1,12 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { prisma } from "./src/lib/prisma";
+
+export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+});
